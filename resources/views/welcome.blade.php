bea05853c995d71280b04e465fa601299d3f45a0
Gustavo Lemos da Silva <lemos.gustavo@aluno.ifsp.edu.br>
2026-05-29 09:46:56 -0300

    Update heading text in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div id="search-container" class="col-md-12">
-    <h1>Busque um evento y</h1>
+    <h1>Busque um evento</h1>
     <form action="">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
